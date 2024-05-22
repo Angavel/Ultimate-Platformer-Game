43aa8bc1e7633f9b17cbbeef75c348b6066879dd
Trent <trent@angavel.com>
2024-05-22 12:03:12 -0400

    Blue box moves up and side to side :)

--- src/Game/GameFrame.java
+++ src/Game/GameFrame.java
@@ -4,62 +4,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.TimerTask;
 import javax.swing.JFrame;
+//import javax.swing.Timer;
+import java.util.Timer;
+import java.util.TimerTask;
 
-class GameFrame extends JFrame implements KeyListener {
-    int x, y; // size of window in pixels
+
+public class GameFrame extends JFrame implements KeyListener{
+    static int xPos, yPos; // position of player in frame
     int time = 200; //how quickly main timer loops
-    boolean gameOver = false;
+//    boolean gameOver = false;
+
+    JLabel playerSprite;
+
+
 
     public GameFrame(int x, int y) {
-        this.x = x;
-        this.y = y;
+        xPos = 235; //default player start position
+        yPos = 700;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("ULTIMATE PLATFORMER GAME");
-        this.setSize(this.x, this.y); //stackoverflow says delete this?
+        this.setSize(x, y);
         this.setLayout(null); ////
         this.addKeyListener(this);
-        gameOver = false; // Resets the game over token, stops infinite looping
+//        gameOver = false; // Resets the game over token, stops infinite looping
 
-        JLabel playerSprite = new JLabel();
-        playerSprite.setBounds(200, 200, 30, 30);
+
+        this.setVisible(true);
+
+
+        playerSprite = new JLabel();
+        this.add(playerSprite);
+        playerSprite.setBounds(xPos, yPos, 30, 30);
 //        playerSprite.setIcon(snek);
         playerSprite.setBackground(Color.BLUE);
         playerSprite.setOpaque(true);
-
-        JButton restartButton = new JButton();
-        restartButton.setText("Restart?"); ////////////////////button no work yet
-        restartButton.setBounds(300, 300, 100, 100);
-//        restartButton.setAction(this.setGameOver(true));
-        restartButton.setEnabled(false);
-        restartButton.setVisible(false);
-
-        this.add(restartButton);
-        this.add(playerSprite);
-
-        this.setVisible(true);
-    }
+        playerSprite.setVisible(true);
 
 
 
+
+//        this.add(restartButton);
+//        restartButton = new JButton();
+//        restartButton.setText("Restart?"); ////////////////////button no work yet
+//        restartButton.setBounds(300, 300, 100, 100);
+////        restartButton.setAction(this.setGameOver(true));
+//        restartButton.setEnabled(false);
+//        restartButton.setVisible(false);
+
+
+    } // GameFrame constructor
+
+    @Override
     public void keyTyped(KeyEvent e) {
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W -> {
-                // logic for jumping up?
-            }// does a thing when W pressed
-            case KeyEvent.VK_S -> {
-                //falling through platforms (maybe too ambitious)
+                jump("J");
             }
-            case KeyEvent.VK_A -> {
-                //moving left
+            case KeyEvent.VK_A -> { //moving left
+                move("L");
             }
-            case KeyEvent.VK_D -> {
-                //moving right
-
+            case KeyEvent.VK_D -> { //moving right
+                move("R");
             }
         }
     }
@@ -67,15 +79,71 @@ class GameFrame extends JFrame implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
         // logic for falling down/stopping?
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W -> {
+                jump("");
+            }
+            case KeyEvent.VK_A -> { //moving left
+                move("");
+            }
+            case KeyEvent.VK_D -> { //moving right
+                move("");
+            }
+        }
     }
 
 
-    public int getX() {
-        return x;
+    public int get_xPos(){
+        return xPos;
     }
 
-    public int getY() {
-        return y;
+    public int get_yPos(){
+        return yPos;
     }
+
+
+
+
+    ///////////////////////MOVEMENT COMMANDS
+    public void jump(String s){
+        if (s.equals("J")) {
+
+            Timer t = new Timer();
+
+            t.scheduleAtFixedRate(new TimerTask() { // creates a persistent timer instance
+                public void run(){
+                System.out.println("TIMER LOOP");
+            } //run method for timer
+
+
+
+            }/*timer task*/,200, 200);
+
+            yPos -= 20;
+            playerSprite.setLocation(xPos, yPos);
+
+        } //if statement
+    }// jump method
+
+
+
+    public void fall() {
+        if (yPos < 700) {
+            yPos -= 1;
+        }
+    }
+
+    public void move(String s){ //TRUE is right FALSE is left
+        if (s.equals("R")){
+            xPos += 5;
+            playerSprite.setLocation(xPos, yPos);
+        } else  if (s.equals("L")){
+            xPos -= 5;
+            playerSprite.setLocation(xPos, yPos);
+        }
+    }
+//////////////////////////
+
+
 
 } //GameFrame class
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,17 +1,27 @@
 package Game;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
 
-        GameFrame window = new GameFrame(500, 800);
-        Player player = new Player(10, 10, 10);
-        int floor = 50; // lowest point player will fall to
+        GameFrame w = new GameFrame(500, 800);
+//        Player player = new Player(10, 10, 10);
+//        int floor = w.get_yPos(); // lowest point player will fall to
 
 
-        //need player to FALL by default if not on bottom.
-        if (window.getX() > floor) {
-            player.goDown();
-        }
+
+
+
+
+//        w.fall(); //put this in the game loop
+//        //need player to FALL by default if not on bottom.
+//        if (w.getX() > floor) {
+//            w.fall();
+
 
 
     } // main method
